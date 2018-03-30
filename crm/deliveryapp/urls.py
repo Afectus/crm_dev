11c33031820f09d3be0d955efce2324c65dd6e43
affectus <gren911@yandex.ru>
2018-03-30 17:04:46 +0700

    Этот урл больше не нужен

--- crm/deliveryapp/urls.py
+++ crm/deliveryapp/urls.py
@@ -6,7 +6,6 @@ from .views import *
 urlpatterns = [
 	# views
 	re_path('^deliveryapp/dellist/list/?$', login_required(dellist_list.as_view()), name='dellist_list'),
-	re_path('^deliveryapp/dellist/my-list/?$', login_required(dellist_mylist.as_view()), name='dellist_mylist'),
 	re_path('^deliveryapp/dellist/add/?$', login_required(dellist_add.as_view()), name='dellist_add'),
 	re_path('^deliveryapp/dellist/(?P<pk>\d+)/?$', login_required(dellist_detail.as_view()), name='dellist_detail'),
 	re_path('^deliveryapp/dellist/edit/(?P<pk>\d+)/?$', login_required(dellist_edit.as_view()), name='dellist_edit'),
